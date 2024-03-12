899f4c41243a98b84e686b5690e2d5455b6ee723
daavid-om <145343929+daavid-om@users.noreply.github.com>
2024-03-12 13:35:29 +0100

    Add files via upload

---
+++ Ejercicio8ReemplazoCaracter/Ejercicio8ReemplazoCaracter/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Ejercicio8ReemplazoCaracter
+{
+    /// <summary>
+    /// Clase principal de la aplicación
+    /// 120324 - dom
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Método principal de la aplicación
+        /// 120324 - dom
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("Ingrese una cadena:");
+            string cadena = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el carácter a reemplazar:");
+            char charOriginal = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            Console.WriteLine("Ingrese el nuevo carácter:");
+            char charReemplazo = Console.ReadKey().KeyChar;
+            Console.WriteLine(); 
+
+            string cadenaReemplazada = ReemplazarCaracter(cadena, charOriginal, charReemplazo);
+            Console.WriteLine("Cadena con reemplazo:");
+            Console.WriteLine(cadenaReemplazada);
+        }
+
+        static string ReemplazarCaracter(string cadena, char charOriginal, char charReemplazo)
+        {
+            char[] cadenaArray = cadena.ToCharArray();
+            for (int i = 0; i < cadenaArray.Length; i++)
+            {
+                if (cadenaArray[i] == charOriginal)
+                {
+                    cadenaArray[i] = charReemplazo;
+                }
+            }
+            return new string(cadenaArray);
+        }
+    }
+}
